0650522546358b062809be52da4342f3d631df03
DarioGiusino <dario.giusino@gmail.com>
2023-05-22 15:20:43 +0200

    3 milestone

--- src/main/java/org/java/main/Main.java
+++ src/main/java/org/java/main/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -13,7 +14,7 @@ public class Main {
 		String user = "root";
 		String password = "root";
 		
-		try(Connection conn = DriverManager.getConnection(url, user, password)){
+		try(Scanner sc = new Scanner(System.in); Connection conn = DriverManager.getConnection(url, user, password)){
 			
 			String sql = "SELECT c.name AS nomeNazione, c.country_id AS idNazione, r.name AS nomeRegione, c2.name AS nomeContinente\r\n"
 					+ "FROM countries c \r\n"
@@ -21,17 +22,26 @@ public class Main {
 					+ "ON c.region_id = r.region_id \r\n"
 					+ "JOIN continents c2 \r\n"
 					+ "ON r.continent_id = c2.continent_id \r\n"
-					+ "ORDER BY c.name ";
+					+ "WHERE c.name LIKE ?\r\n"
+					+ "ORDER BY c.name";
 			
-			try(PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()){
+			System.out.print("Inserisci parametro di ricerca:");
+			String searchTerm = sc.nextLine();
+			
+			try(PreparedStatement ps = conn.prepareStatement(sql)){
 				
-				while(rs.next()) {
-					final String countryName = rs.getString("nomeNazione");
-					final int countryId = rs.getInt("idNazione");
-					final String regionName = rs.getString("nomeRegione");
-					final String continentName = rs.getString("nomeContinente");
+				ps.setString(1, "%" + searchTerm + "%");
+				
+				try(ResultSet rs = ps.executeQuery()){
 					
-					System.out.println(countryName + " - " + countryId + " - " + regionName + " - " + continentName);
+					while(rs.next()) {
+						final String countryName = rs.getString("nomeNazione");
+						final int countryId = rs.getInt("idNazione");
+						final String regionName = rs.getString("nomeRegione");
+						final String continentName = rs.getString("nomeContinente");
+						
+						System.out.println(countryName + " - " + countryId + " - " + regionName + " - " + continentName);
+					}
 				}
 				
 			}
